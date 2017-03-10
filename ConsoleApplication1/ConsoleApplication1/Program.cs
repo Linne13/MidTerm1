a4cb2582716ed6c51d4c0bc0fe72bcb629770c47
Aleksandra E. Khan <a_khan@kbtu.kz>
2017-03-10 10:09:39 +0600

    First Task

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = int.Parse(Console.ReadLine());
+            int a;
+            int sum = 0;
+            if (x == 0)
+            {
+                sum = 0;
+            }
+            while (x > 0)
+            {
+                a = x % 10;
+                x = x / 10;
+                sum = sum + a;
+            }
+            Console.WriteLine(sum);
+            Console.ReadKey();
+        }
+    }
+}
