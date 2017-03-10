41960fd148030b28be4520a8ffdcaec9ee51ed6e
Aleksandra E. Khan <a_khan@kbtu.kz>
2017-03-10 10:23:49 +0600

    task 2

---
+++ ConsoleApplication2/ConsoleApplication2/Points.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication2
+{
+    class Points
+    {
+        public int x, y;
+        public Points(int xx, int yy)
+        {
+            x = xx;
+            y = yy;
+        }
+    }
+}
---
+++ ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        class Polygon
+        {
+            public List<Points> body;
+
+          
+            public override string ToString()
+            {
+                return ;
+            }
+        }
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
