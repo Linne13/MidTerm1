37d7f33e70d0d479b375889d226d58417c56d70d
Aleksandra E. Khan <a_khan@kbtu.kz>
2017-03-10 10:42:01 +0600

    task 3

---
+++ ConsoleApplication3/ConsoleApplication3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace ConsoleApplication3
+{
+    class Program
+    {
+        public static void Print()
+        {
+            Thread.Sleep(100);
+        }
+
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
---
+++ ConsoleApplication3/ConsoleApplication4/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace ConsoleApplication4
+{
+    class Program
+    {
+        public void Info(DirectoryInfo directory)
+        {
+
+        }
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
