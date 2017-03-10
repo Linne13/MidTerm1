39b4bb44a672642426fab64a2fedae930a83444c
Aleksandra E. Khan <a_khan@kbtu.kz>
2017-03-10 10:51:13 +0600

    TASK 3

---
+++ Light/Light/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace Light
+{
+    class Program
+    {
+        public static void Print1()
+        {
+            Thread.Sleep(100);
+
+            Console.ForegroundColor = ConsoleColor.Red;
+
+            Console.WriteLine("****");
+            Console.WriteLine("****");
+            Console.WriteLine("****");
+            Console.WriteLine("****");
+        }
+
+        public static void Print2()
+        {
+            Thread.Sleep(100);
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+
+            Console.WriteLine("****");
+            Console.WriteLine("****");
+            Console.WriteLine("****");
+            Console.WriteLine("****");
+        }
+
+        public static void Print3()
+        {
+            Thread.Sleep(100);
+
+            Console.ForegroundColor = ConsoleColor.Green;
+
+            Console.WriteLine("****");
+            Console.WriteLine("****");
+            Console.WriteLine("****");
+            Console.WriteLine("****");
+        }
+
+
+        static void Main(string[] args)
+        {
+            Print1();
+            Console.WriteLine("\n");
+            Print2();
+            Console.WriteLine("\n");
+            Print3();
+            Console.ReadKey();
+
+            
+
+        }
+    }
+}
